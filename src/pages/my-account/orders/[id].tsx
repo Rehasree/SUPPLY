9760f1d7d30bbd3dd48f13365b17e988f42e9f4b
Rehasree <82755435+gr8stuff@users.noreply.github.com>
2021-12-03 16:49:17 +0530

    added firebase

--- src/pages/my-account/orders/[id].tsx
+++ src/pages/my-account/orders/[id].tsx
@@ -1,13 +1,13 @@
 import Layout from "@components/layout/layout";
 import AccountLayout from "@components/my-account/account-layout";
-import OrderDetails from "@components/order/order-details";
+// import OrderDetails from "@components/order/order-details";
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export default function OrderPage() {
 	return (
 		<AccountLayout>
-			<OrderDetails className="p-0" />
+			{/* <OrderDetails className="p-0" /> */}
 		</AccountLayout>
 	);
 }
